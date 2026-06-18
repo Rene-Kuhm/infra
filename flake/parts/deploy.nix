2924601d1123f9c5d791fc8e42f8d627317214b1
Rene-Kuhm <rene-kuhm@users.noreply.github.com>
2026-06-18 19:53:38 -0300

    fix(deploy): use inputs.deploy-rs.lib in path reference

--- flake/parts/deploy.nix
+++ flake/parts/deploy.nix
@@ -5,12 +5,9 @@
       profiles.system = {
         user = "root";
         sshUser = "root";
-        path = deploy-rs.lib.x86_64-linux.activate.${self.nixosConfigurations.tecsnosquire};
-        # Replace this with your public SSH key after first deploy
+        path = inputs.deploy-rs.lib.x86_64-linux.activate.${self.nixosConfigurations.tecsnosquire};
         sshOpts = [ "-o" "StrictHostKeyChecking=accept-new" ];
       };
     };
-  } // {
-    inherit (inputs.deploy-rs) deploy;
   };
 }
