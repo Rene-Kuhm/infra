9ab56bf6caf1ecc0133ee913c8ced29ecc4084e3
Rene-Kuhm <rene-kuhm@users.noreply.github.com>
2026-06-18 20:48:05 -0300

    fix(ssh): correct option name Macs (not MACs)

--- modules/base/ssh.nix
+++ modules/base/ssh.nix
@@ -30,7 +30,7 @@
         "aes256-gcm@openssh.com"
         "aes128-gcm@openssh.com"
       ];
-      MACs = [
+      Macs = [
         "hmac-sha2-512-etm@openssh.com"
         "hmac-sha2-256-etm@openssh.com"
       ];
