d6cd8902ad53824fc37d82a294dd4d862cf12148
Rene-Kuhm <rene-kuhm@users.noreply.github.com>
2026-06-18 20:10:19 -0300

    fix(boot): remove invalid pcrBanks option (only enable works in lanzaboote 0.4.x)

--- hosts/tecsnosquire/boot.nix
+++ hosts/tecsnosquire/boot.nix
@@ -2,13 +2,9 @@
 
 {
   # Lanzaboote: signed UKI boot
-  # Note: enableSecureBoot is set to true AFTER first install + MOK enrollment
-  boot.lanzaboote = {
-    enable = true;
-    pcrBanks = [ "sha256" ];
-  };
+  boot.lanzaboote.enable = true;
 
-  # Console setup
+  # Console
   boot.consoleLogLevel = 0;
   boot.initrd.verbose = false;
 
@@ -16,7 +12,5 @@
   boot.kernelParams = [
     "quiet"
     "loglevel=3"
-    "rd.systemd.show_status=false"
-    "udev.log_level=3"
   ];
 }
